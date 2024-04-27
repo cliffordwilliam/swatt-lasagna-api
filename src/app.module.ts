78d35b69883c1da1d80064046fca2a2c9c397b1b
Clifford William <ccliffordwilliam@gmail.com>
2024-04-27 20:30:32 +0800

    feat(config): configures config module
    configures config module to read .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ConfigModule.forRoot(), ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
