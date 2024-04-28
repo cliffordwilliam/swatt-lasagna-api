510c0318a668ded2b3d548bc70309a2422d1024e
Clifford William <ccliffordwilliam@gmail.com>
2024-04-28 14:42:25 +0800

    fix(seed): commented out all seed
    commented out all seed to prevent data overrides in migration to remote or local

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,7 +11,6 @@ async function main() {
   //     { nama: 'Lasagna', tipe: 'Extra Family', harga: 550.0 },
   //     { nama: 'Lasagna', tipe: 'Party Medium', harga: 1300.0 },
   //     { nama: 'Lasagna', tipe: 'Party Large', harga: 2400.0 },
-
   //     { nama: 'Makaroni', tipe: 'Mini', harga: 50.0 },
   //     { nama: 'Makaroni', tipe: 'Small', harga: 85.0 },
   //     { nama: 'Makaroni', tipe: 'Oval', harga: 110.0 },
@@ -22,7 +21,6 @@ async function main() {
   //     { nama: 'Makaroni', tipe: 'Extra Family', harga: 435.0 },
   //     { nama: 'Makaroni', tipe: 'Party Medium', harga: 1000.0 },
   //     { nama: 'Makaroni', tipe: 'Party Large', harga: 2000.0 },
-
   //     { nama: 'MarbelCake', tipe: '1 Loyang', harga: 330.0 },
   //     { nama: 'MarbelCake', tipe: '1 Potong', harga: 22.0 },
   //     { nama: 'MarbelCake', tipe: '3 Potong', harga: 63.0 },
@@ -31,50 +29,37 @@ async function main() {
   //     { nama: 'MarbelCake', tipe: '9 Potong', harga: 185.0 },
   //     { nama: 'MarbelCake', tipe: '12 Potong', harga: 245.0 },
   //     { nama: 'MarbelCake', tipe: 'Hampers', harga: 345.0 },
-
   //     { nama: 'Nastar', tipe: 'Toples Bulat', harga: 170.0 },
   //     { nama: 'Nastar', tipe: 'Toples Kotak', harga: 135.0 },
-
   //     { nama: 'Kastengles', tipe: 'Toples Bulat', harga: 195.0 },
   //     { nama: 'Kastengles', tipe: 'Toples Kotak', harga: 135.0 },
-
   //     { nama: 'Lidah Kucing', tipe: 'Toples Bulat', harga: 150.0 },
   //     { nama: 'Lidah Kucing', tipe: 'Toples Kotak', harga: 120.0 },
-
   //     { nama: 'Almond Keju', tipe: 'Toples Bulat', harga: 170.0 },
   //     { nama: 'Almond Keju', tipe: 'Toples Kotak', harga: 135.0 },
-
   //     { nama: 'Sagu Keju', tipe: 'Toples Bulat', harga: 150.0 },
   //     { nama: 'Sagu Keju', tipe: 'Toples Kotak', harga: 120.0 },
-
   //     { nama: 'Cheese Stick', tipe: 'Toples Kotak', harga: 150.0 },
-
   //     { nama: 'Biscoti', tipe: 'Toples Kotak', harga: 120.0 },
-
   //     { nama: 'Bolu Peuyeum', tipe: 'Biasa', harga: 125.0 },
-
   //     { nama: 'Hampers Cookies', tipe: 'K3', harga: 490.0 },
   //     { nama: 'Hampers Cookies', tipe: 'K4', harga: 630.0 },
   //     { nama: 'Hampers Cookies', tipe: 'K5', harga: 810.0 },
   //     { nama: 'Hampers Cookies', tipe: 'B3', harga: 615.0 },
   //     { nama: 'Hampers Cookies', tipe: 'B4', harga: 795.0 },
-
   //     { nama: 'Roti', tipe: 'Baso', harga: 16.0 },
   //     { nama: 'Roti', tipe: 'Keju', harga: 16.0 },
   //     { nama: 'Roti', tipe: 'Coklat', harga: 16.0 },
   //     { nama: 'Roti', tipe: 'Paket 3', harga: 45.0 },
-
   //     { nama: 'Box Hampers', tipe: 'K3', harga: 75.0 },
   //     { nama: 'Box Hampers', tipe: 'K4', harga: 95.0 },
   //     { nama: 'Box Hampers', tipe: 'K5', harga: 85.0 },
   //     { nama: 'Box Hampers', tipe: 'B3', harga: 85.0 },
   //     { nama: 'Box Hampers', tipe: 'B4', harga: 95.0 },
   //     { nama: 'Box Hampers', tipe: 'BL', harga: 85.0 },
-
   //     { nama: 'Pudding', tipe: 'Biasa', harga: 30.0 },
   //     { nama: 'Pudding', tipe: 'Packet 4', harga: 115.0 },
   //     { nama: 'Pudding', tipe: 'Paket 6', harga: 172.5 },
-
   //     { nama: 'Tas', tipe: 'K3', harga: 15.0 },
   //     { nama: 'Tas', tipe: 'K4', harga: 15.0 },
   //     { nama: 'Tas', tipe: 'K5', harga: 15.5 },
@@ -82,20 +67,13 @@ async function main() {
   //     { nama: 'Tas', tipe: 'B4', harga: 15.5 },
   //     { nama: 'Tas', tipe: 'MC', harga: 15.5 },
   //     { nama: 'Tas', tipe: 'L', harga: 15.5 },
-
   //     { nama: 'Air mineral', tipe: 'Biasa', harga: 5.0 },
-
   //     { nama: 'Poka', tipe: 'Biasa', harga: 12.0 },
-
   //     { nama: 'Kripik Cherry', tipe: 'Biasa', harga: 28.0 },
-
   //     { nama: 'Krupuk Akar', tipe: 'Biasa', harga: 28.0 },
-
   //     { nama: 'Sambel', tipe: 'Biasa', harga: 30.5 },
   //   ];
-
   //   const createdItems = [];
-
   //   for (const itemData of itemsData) {
   //     const createdItem = await prisma.item.create({
   //       data: itemData,
@@ -103,7 +81,6 @@ async function main() {
   //     createdItems.push(createdItem);
   //   }
   //   console.log('Seeded items:', createdItems);
-
   //   const pickupDeliveries = [
   //     { name: 'Pickup' },
   //     { name: 'Delivery' },
@@ -116,9 +93,7 @@ async function main() {
   //     { name: 'Bineka' },
   //     { name: 'Jne' },
   //   ];
-
   //   const createdItems = [];
-
   //   for (const pickupDelivery of pickupDeliveries) {
   //     const createdItem = await prisma.pickupDelivery.create({
   //       data: pickupDelivery,
@@ -126,16 +101,13 @@ async function main() {
   //     createdItems.push(createdItem);
   //   }
   //   console.log('Seeded items:', createdItems);
-
   //   const Pembayarans = [
   //     { name: 'Tunai' },
   //     { name: 'Kartu Kredit' },
   //     { name: 'Qr' },
   //     { name: 'Transfer' },
   //   ];
-
   //   const createdItems = [];
-
   //   for (const pembayaran of Pembayarans) {
   //     const createdItem = await prisma.pembayaran.create({
   //       data: pembayaran,
@@ -143,18 +115,15 @@ async function main() {
   //     createdItems.push(createdItem);
   //   }
   //   console.log('Seeded items:', createdItems);
-
-  const Roles = [{ name: 'Admin' }, { name: 'Karyawan' }];
-
-  const createdItems = [];
-
-  for (const role of Roles) {
-    const createdItem = await prisma.role.create({
-      data: role,
-    });
-    createdItems.push(createdItem);
-  }
-  console.log('Seeded items:', createdItems);
+  //   const Roles = [{ name: 'Admin' }, { name: 'Karyawan' }];
+  //   const createdItems = [];
+  //   for (const role of Roles) {
+  //     const createdItem = await prisma.role.create({
+  //       data: role,
+  //     });
+  //     createdItems.push(createdItem);
+  //   }
+  //   console.log('Seeded items:', createdItems);
 }
 main()
   .then(async () => {
