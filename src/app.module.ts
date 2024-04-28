f39e5d14f93697151ddf98509b49f30f7549100e
Clifford William <ccliffordwilliam@gmail.com>
2024-04-28 23:40:23 +0800

    feat(exception-filter): integrates nestjs-prisma exception filter
    integrates nestjs-prisma exception filter for bad_request, conflict and not_found

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { HttpStatus, Module } from '@nestjs/common';
+import { providePrismaClientExceptionFilter } from 'nestjs-prisma';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +9,13 @@ import { RolesModule } from './roles/roles.module';
 @Module({
   imports: [ConfigModule.forRoot(), ItemsModule, RolesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    providePrismaClientExceptionFilter({
+      P2000: HttpStatus.BAD_REQUEST,
+      P2002: HttpStatus.CONFLICT,
+      P2025: HttpStatus.NOT_FOUND,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpStatus } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(
+    new PrismaClientExceptionFilter(httpAdapter, {
+      P2000: HttpStatus.BAD_REQUEST,
+      P2002: HttpStatus.CONFLICT,
+      P2025: HttpStatus.NOT_FOUND,
+    }),
+  );
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +23,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
