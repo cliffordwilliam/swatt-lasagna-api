da54bc70db03dc46d578b1cacef1cb484d171370
Clifford William <ccliffordwilliam@gmail.com>
2024-05-03 06:08:49 +0800

    feat(cors): vercel cors
    vercel cors

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
 
   app.use(helmet());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://swatt-lasagna-app.vercel.app'],
   });
   app.use(logger);
 
