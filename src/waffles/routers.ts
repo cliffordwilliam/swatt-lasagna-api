e322a51646a1b7293afff2b959b715cf78626d1b
Clifford William <ccliffordwilliam@gmail.com>
2025-11-04 03:01:37 +0700

    fix: lints

--- src/waffles/routers.ts
+++ src/waffles/routers.ts
@@ -5,8 +5,8 @@ import { ManageWaffle } from "./services/manage_waffle";
 
 const waffleRouter = Router();
 
-waffleRouter.post("/", validateBody(WaffleCreateRequest), async (req, res, next) => {
-    const created_waffle = await ManageWaffle.create(req.body);
+waffleRouter.post("/", validateBody(WaffleCreateRequest), async (req, res, _next) => {
+    const created_waffle = ManageWaffle.create(req.body);
     res.json(validateResponse(CreateWaffleResponse, created_waffle));
 });
 
