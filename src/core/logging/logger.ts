48f2de8aa2a9d07699f9838df65fcf75f5ff1e1b
Clifford William <ccliffordwilliam@gmail.com>
2025-11-17 03:28:14 +0700

    fix: simplify pino

--- src/core/logging/logger.ts
+++ src/core/logging/logger.ts
@@ -4,11 +4,6 @@ export default process.stdout.isTTY
   ? pino({
       transport: {
         target: "pino-pretty",
-        options: {
-          colorize: true,
-          translateTime: "HH:MM:ss Z",
-          ignore: "pid,hostname",
-        },
       },
     })
   : pino();
